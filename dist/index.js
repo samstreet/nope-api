5db428801476e249f257a0f2e230021748931289
Sam Street <samstreet@null.net>
2018-10-26 20:11:23 +0100

    removed dist file

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const App_1 = require("./App");
-let api = new App_1.App(express(), 3333);
-api.run();
-//# sourceMappingURL=index.js.map
